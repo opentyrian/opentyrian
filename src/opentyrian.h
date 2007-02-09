e9d7bb929addcc26f80f6d2164e95afb81c92eb3
Carl Reinke <carlreinke@users.noreply.github.com>
2007-02-09 22:30:03 +0000

    added global types and includes

---
+++ src/opentyrian.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <SDL/SDL.h>
+
+typedef signed long JE_longint;
+typedef signed short JE_integer;
+typedef unsigned short JE_word;
+typedef unsigned char JE_byte;
+typedef int JE_boolean;
