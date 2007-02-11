dacc598b28e617dda0837f5aa9d788070285c38b
Carl Reinke <carlreinke@users.noreply.github.com>
2007-02-11 05:48:26 +0000

    added "include guards"

--- src/opentyrian.h
+++ src/opentyrian.h
@@ -1,3 +1,6 @@
+#ifndef _OPENTYRIAN_H
+#define _OPENTYRIAN_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,3 +12,7 @@ typedef signed char JE_shortint;
 typedef unsigned short JE_word;
 typedef unsigned char JE_byte;
 typedef int JE_boolean;
+
+SDL_Surface *vgascreenseg = NULL;
+
+#endif /* _OPENTYRIAN_H */
