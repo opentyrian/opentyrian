5dbb20a45fc1cd13ee5fc04234e033c1ac835d6d
Carl Reinke <carlreinke@users.noreply.github.com>
2007-02-11 01:33:43 +0000

    added typedef of JE_shortint

--- src/opentyrian.h
+++ src/opentyrian.h
@@ -5,6 +5,7 @@
 
 typedef signed long JE_longint;
 typedef signed short JE_integer;
+typedef signed char JE_shortint;
 typedef unsigned short JE_word;
 typedef unsigned char JE_byte;
 typedef int JE_boolean;
