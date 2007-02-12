726b3220b8ac00dc3cf99a9aff27cce0296bbbeb
Carl Reinke <carlreinke@users.noreply.github.com>
2007-02-12 03:59:19 +0000

    truncating opentyrian.h

---
+++ src/opentyr.h
@@ -0,0 +1,21 @@
+#ifndef _OPENTYRIAN_H
+#define _OPENTYRIAN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <SDL/SDL.h>
+
+typedef signed long JE_longint;
+typedef signed short JE_integer;
+typedef signed char JE_shortint;
+typedef unsigned short JE_word;
+typedef unsigned char JE_byte;
+typedef int JE_boolean;
+
+typedef char *JE_string;
+typedef char JE_char;
+
+SDL_Surface *vgascreenseg;
+
+#endif /* _OPENTYRIAN_H */
