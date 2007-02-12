f7aa998b1a864ffecafe84b52f781717ea75316a
syntaxglitch <none@none>
2007-02-12 04:07:47 +0000

    Makefile works, added stub main

--- Makefile
+++ Makefile
@@ -1,1 +1,21 @@
+CC = gcc
+CFLAGS = -ansi -pedantic -Wall -Werror
+LDFLAGS = `sdl-config --cflags --libs`
+IN = src
+OUT = bin
+OBJS = opentyr.o newshape.o fonthand.o
 
+all: tyrian
+
+.PHONY: clean
+clean:
+	-rm ${IN}/*.o
+	-rm ${OUT}/tyrian
+	-rmdir ${OUT}
+
+tyrian: ${OBJS}
+	-mkdir ${OUT} 
+	${CC} ${LDFLAGS} -o ${OUT}/$@ $(OBJS:%=${IN}/%)
+
+${OBJS}:
+	${CC} ${CFLAGS} -c ${IN}/$*.c -o ${IN}/$*.o
---
+++ src/opentyr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include "opentyr.h"
+
+int main()
+{
+	/* TODO: DetectCFG */
+	/* TODO: scanforepisodes */
+
+	printf("\nWelcome to... >> Open Tyrian v0.1 <<\n\n");
+
+	/* TODO: InitKeyboard */
+
+	/* TODO: Not sure what these are about?
+	 * recordfilenum = 1;
+	 * playdemonum = 0; 
+	 * playdemo = false; */
+
+	/* TODO: LoadConfiguration */
+
+	/* TODO: Tyrian originally checked availible memory here. */
+
+	/* here ends line 92771 of TYRIAN2.PAS
+	 * TODO: Finish it and stuff. */
+	
+	return 0;
+}
