c430cde1dfc9dbe4eabaceaf5eb7245987da7742
Yuri Kunde Schlesner <yuriks@yuriks.net>
2007-02-24 04:48:12 +0000

    Added some comments and fixes

--- src/vga256d.h
+++ src/vga256d.h
@@ -3,10 +3,10 @@
 
 #include "opentyr.h"
 
-typedef JE_word shape16B[1];
+typedef JE_word shape16B[1]; /* [0.. 0] */
 typedef shape16B *shape16;
-typedef JE_word shapetypeone[84];
-typedef JE_byte screentype[65535];
+typedef JE_word shapetypeone[84]; /* [1..84] */
+typedef JE_byte screentype[65535]; /* [0..65534] */
 typedef screentype *screenptr;
 
 void initvga256( void );
@@ -36,15 +36,15 @@ void barshade2( JE_word a, JE_word b, JE_word c, JE_word d );
 void barbright( JE_word a, JE_word b, JE_word c, JE_word d );
 void circle( JE_word x, JE_byte y, JE_word z, JE_byte c );
 void line( JE_word a, JE_byte b, JE_longint c, JE_byte d, JE_byte e );
-void getimage16( JE_word a, JE_byte b, shape16B p );
-void putimage16( JE_word a, JE_byte b, shape16B p );
+void getimage16( JE_word a, JE_byte b, shape16B *p );
+void putimage16( JE_word a, JE_byte b, shape16B *p );
 void drawgraphic( JE_word x, JE_word y, shapetypeone s );
 void ABSdrawgraphic( shapetypeone s );
 void drawgraphicover( JE_word x, JE_word y, shapetypeone s );
 void ABSdrawgraphicover( shapetypeone s );
 void readgraphic( JE_integer x, JE_integer y, shapetypeone s );
-void getk( JE_byte *y );
-void getupk( JE_byte *k );
+void getk( JE_char *y );
+void getupk( JE_char *k );
 JE_boolean keypressed( JE_char *kp );
 JE_boolean kp( void );
 void wait( JE_byte min, JE_byte sec, JE_byte hun );
