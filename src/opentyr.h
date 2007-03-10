f7cd35cb68d2f5452f4d65eac9b6d514a6ce2219
Yuri Kunde Schlesner <yuriks@yuriks.net>
2007-03-10 20:21:57 +0000

    Added INLINE conditional define to opentyr.h

--- src/opentyr.h
+++ src/opentyr.h
@@ -6,6 +6,14 @@
 
 #include <SDL/SDL.h>
 
+#if __STDC_VERSION__ >= 199901L
+#define INLINE inline
+#elif defined __GNUC__
+#define INLINE __inline__
+#else
+#define INLINE
+#endif
+
 typedef signed long JE_longint;
 typedef signed short JE_integer;
 typedef signed char JE_shortint;
