14a197d28d3dee4abc04b46a7eae16d98930b78f
Yuri Kunde Schlesner <yuriks@yuriks.net>
2007-03-19 02:14:38 +0000

    Added text loading to helptext.c

--- src/helptext.c
+++ src/helptext.c
@@ -23,6 +23,8 @@
 
 #include "fonthand.h"
 #include "newshape.h"
+#include "error.h"
+#include "config.h"
 
 #include <string.h>
 
@@ -49,33 +51,33 @@ JE_byte helpboxcolor = 12;
 JE_byte helpboxbrightness = 1;
 JE_byte helpboxshadetype = FullShade;
 
-JE_helptxttype *helptxt;
-char pname[21][16];     /* [1..21] of string [15] */
-char misctext[68][42];      /* [1..68] of string [41] */
-char misctextB[5][11];      /* [1..5] of string [10] */
-char keyname[8][18];        /* [1..8] of string [17] */
-char menutext[7][21];       /* [1..7] of string [20] */
-char outputs[9][31];        /* [1..9] of string [30] */
-char topicname[6][21];      /* [1..6] of string [20] */
-JE_helptexttype *mainmenuhelp;
-char ingametext[6][21];     /* [1..6] of string [20] */
-char detaillevel[6][13];    /* [1..6] of string [12] */
-char gamespeedtext[5][13];  /* [1..5] of string [12] */
-char episodename[6][31];    /* [0..5] of string [30] */
-char difficultyname[7][21]; /* [0..6] of string [20] */
-char playername[5][26];     /* [0..4] of string [25] */
-char inputdevices[3][13];   /* [1..3] of string [12] */
-char networktext[4][21];    /* [1..4] of string [20] */
-char difficultynameB[10][21];   /* [0..9] of string [20] */
-char joybutton[5][21];      /* [1..5] of string [20] */
-char superships[11][26];    /* [0..10] of string [25] */
-char SpecialName[9][10];    /* [1..9] of string [9] */
-JE_destructhelptype *Destructhelp;
-char weaponnames[17][17];   /* [1..17] of string [16] */
-char destructmodename[destructmodes][13];   /* [1..destructmodes] of string [12] */
-JE_shipinfotype *shipinfo;
+char helptxt[maxhelpmessage][231];
+char pname[21][16];          /* [1..21] of string [15] */
+char misctext[68][42];       /* [1..68] of string [41] */
+char misctextB[5][11];       /* [1..5] of string [10] */
+char keyname[8][18];         /* [1..8] of string [17] */
+char menutext[7][21];        /* [1..7] of string [20] */
+char outputs[9][31];         /* [1..9] of string [30] */
+char topicname[6][21];       /* [1..6] of string [20] */
+char mainmenuhelp[34][66];
+char ingametext[6][21];      /* [1..6] of string [20] */
+char detaillevel[6][13];     /* [1..6] of string [12] */
+char gamespeedtext[5][13];   /* [1..5] of string [12] */
+char episodename[6][31];     /* [0..5] of string [30] */
+char difficultyname[7][21];  /* [0..6] of string [20] */
+char playername[5][26];      /* [0..4] of string [25] */
+char inputdevices[3][13];    /* [1..3] of string [12] */
+char networktext[4][21];     /* [1..4] of string [20] */
+char difficultynameB[10][21];/* [0..9] of string [20] */
+char joybutton[5][21];       /* [1..5] of string [20] */
+char superships[11][26];     /* [0..10] of string [25] */
+char specialname[9][10];     /* [1..9] of string [9] */
+char destructhelp[25][22];
+char weaponnames[17][17];    /* [1..17] of string [16] */
+char destructmodename[destructmodes][13]; /* [1..destructmodes] of string [12] */
+char shipinfo[13][2][256];
+char menuint[maxmenu+1][11][18]; /* [0..maxmenu, 1..11] of string [17] */
 
-char menuint[maxmenu+1][11][18];    /* [0..maxmenu, 1..11] of string [17] */
 
 JE_byte temp, temp2;
 
@@ -134,14 +136,182 @@ void JE_HelpBox( JE_word x, JE_word y, JE_string message, JE_byte boxwidth )
 
 void JE_HBox(JE_word x, JE_word y, JE_byte messagenum, JE_byte boxwidth)
 {
-    JE_HelpBox(x, y, (*helptxt)[messagenum], boxwidth);
+    JE_HelpBox(x, y, helptxt[messagenum], boxwidth);
 }
 
-
-void helptext_init(void)
+void JE_loadhelptext( void )
 {
-  helptxt = malloc(sizeof(JE_helptxttype));
-  mainmenuhelp = malloc(sizeof(JE_helptxttype));
-  Destructhelp = malloc(sizeof(JE_destructhelptype));
-  shipinfo = malloc(sizeof(JE_shipinfotype));
+    FILE *f; int i;
+    JE_longint Episode1DataLoc; /* TODO: REMOVE BECAUSE IT BELONGS IN EPISODES.PAS */
+
+    JE_resetfileext(&f, "TYRIAN.HDT", FALSE);
+    fread(&Episode1DataLoc, sizeof(Episode1DataLoc), 1, f);
+    JE_skipcryptln(f);
+
+    for (i = 0; i < maxhelpmessage; i++) {
+        JE_readcryptln(f,helptxt[i]);           /*Online Help*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 21; i++) {
+        JE_readcryptln(f,pname[i]);             /*Planet names*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 68; i++) {
+        JE_readcryptln(f,misctext[i]);          /*Miscellaneous text*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 5; i++) {
+        JE_readcryptln(f,misctextB[i]);         /*Little Miscellaneous text*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 11; i++) {
+        JE_readcryptln(f,menuint[6][i]);        /*Key names*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 7; i++) {
+        JE_readcryptln(f,menutext[i]);          /*Main Menu*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 9; i++) {
+        JE_readcryptln(f,outputs[i]);           /*Event text*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 6; i++) {
+        JE_readcryptln(f,topicname[i]);         /*Help topics*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 34; i++) {
+        JE_readcryptln(f,mainmenuhelp[i]);      /*Main Menu Help*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+
+    for (i = 0; i < 7; i++) {
+        JE_readcryptln(f,menuint[1][i]);        /*Menu 1 - Main*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 9; i++) {
+        JE_readcryptln(f,menuint[2][i]);        /*Menu 2 - Items*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 8; i++) {
+        JE_readcryptln(f,menuint[3][i]);        /*Menu 3 - Options*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 6; i++) {
+        JE_readcryptln(f,ingametext[i]);        /*InGame Menu*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 6; i++) {
+        JE_readcryptln(f,detaillevel[i]);       /*Detail Level*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 5; i++) {
+        JE_readcryptln(f,gamespeedtext[i]);     /*Game speed text*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 5; i++) {
+        JE_readcryptln(f,episodename[i]);       /*Episode Names*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 6; i++) {
+        JE_readcryptln(f,difficultyname[i]);    /*Difficulty Level*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 4; i++) {
+        JE_readcryptln(f,playername[i]);        /*Player Names*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 6; i++) {
+        JE_readcryptln(f,menuint[10][i]);       /*Menu 10 - 2Player Main*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 3; i++) {
+        JE_readcryptln(f,inputdevices[i]);      /*Input Devices*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 4; i++) {
+        JE_readcryptln(f,networktext[i]);       /*Network text*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 4; i++) {
+        JE_readcryptln(f,menuint[11][i]);       /*Menu 11 - 2Player Network*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 10; i++) {
+        JE_readcryptln(f,difficultynameB[i]);   /*HighScore Difficulty Names*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 6; i++) {
+        JE_readcryptln(f,menuint[12][i]);       /*Menu 12 - Network Options*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 7; i++) {
+        JE_readcryptln(f,menuint[13][i]);       /*Menu 13 - Joystick*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 5; i++) {
+        JE_readcryptln(f,joybutton[i]);         /*Joystick Button Assignments*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);                                                                                                                                  
+    
+    for (i = 0; i < 10; i++) {
+        JE_readcryptln(f,superships[i]);        /*SuperShips - For Super Arcade Mode*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 9; i++) {
+        JE_readcryptln(f,specialname[i]);       /*SuperShips - For Super Arcade Mode*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 25; i++) {
+        JE_readcryptln(f,destructhelp[i]);      /*Secret DESTRUCT game*/                                                                                       
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 17; i++) {
+        JE_readcryptln(f,weaponnames[i]);       /*Secret DESTRUCT weapons*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < destructmodes; i++) {
+        JE_readcryptln(f,destructmodename[i]);  /*Secret DESTRUCT modes*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 13; i++) {
+        JE_readcryptln(f,shipinfo[i][0]);       /*NEW: Ship Info*/
+        JE_readcryptln(f,shipinfo[i][1]);       /*NEW: Ship Info*/
+    }
+    JE_skipcryptln(f); JE_skipcryptln(f);
+    
+    for (i = 0; i < 5; i++) {
+        JE_readcryptln(f,menuint[14][i]);       /*Menu 12 - Network Options*/
+    }
+
+    fclose(f);
 }
--- src/helptext.h
+++ src/helptext.h
@@ -26,15 +26,10 @@
 
 #define destructmodes 5
 
-typedef char JE_helptxttype[maxhelpmessage][231];	/* [1..maxhelpmessage] of string [230] */
-typedef char JE_helptexttype[34][66];	/* [1..34] of string [65] */
-typedef char JE_destructhelptype[25][22];	/* [1..25] of string [21] */
-typedef JE_string JE_shipinfotype[13][2];	/* [1..13, 1..2] */
-
 #ifndef NO_EXTERNS
 extern JE_byte verticalheight;
 extern JE_byte helpboxcolor, helpboxbrightness, helpboxshadetype;
-extern JE_helptxttype *helptxt;
+extern char helptxt[maxhelpmessage][231];
 extern char pname[21][16];          /* [1..21] of string [15] */
 extern char misctext[68][42];       /* [1..68] of string [41] */
 extern char misctextB[5][11];       /* [1..5] of string [10] */
@@ -42,7 +37,7 @@ extern char keyname[8][18];         /* [1..8] of string [17] */
 extern char menutext[7][21];        /* [1..7] of string [20] */
 extern char outputs[9][31];	        /* [1..9] of string [30] */
 extern char topicname[6][21];       /* [1..6] of string [20] */
-extern JE_helptexttype *mainmenuhelp;
+extern char mainmenuhelp[34][66];
 extern char ingametext[6][21];      /* [1..6] of string [20] */
 extern char detaillevel[6][13];     /* [1..6] of string [12] */
 extern char gamespeedtext[5][13];   /* [1..5] of string [12] */
@@ -54,11 +49,11 @@ extern char networktext[4][21];     /* [1..4] of string [20] */
 extern char difficultynameB[10][21];/* [0..9] of string [20] */
 extern char joybutton[5][21];       /* [1..5] of string [20] */
 extern char superships[11][26];     /* [0..10] of string [25] */
-extern char SpecialName[9][10];     /* [1..9] of string [9] */
-extern JE_destructhelptype *Destructhelp;
+extern char specialname[9][10];     /* [1..9] of string [9] */
+extern char destructhelp[25][22];
 extern char weaponnames[17][17];    /* [1..17] of string [16] */
 extern char destructmodename[destructmodes][13]; /* [1..destructmodes] of string [12] */
-extern JE_shipinfotype *shipinfo;
+extern char shipinfo[13][2][256];
 extern char menuint[maxmenu+1][11][18]; /* [0..maxmenu, 1..11] of string [17] */
 extern JE_byte menuhelp[maxmenu][11];   /* [1..maxmenu, 1..11] */
 #endif
