9d312a88e418cd61d8ac1b5db8cd26b200b417f0
Carl Reinke <carlreinke@users.noreply.github.com>
2007-03-23 03:24:08 +0000

    ported config header and variables

--- src/config.c
+++ src/config.c
@@ -17,10 +17,218 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 #include "opentyr.h"
+
+#define NO_EXTERNS
 #include "config.h"
+#undef NO_EXTERNS
 
 #include <stdio.h>
 #include <string.h>
+
+/* Configuration Load/Save handler */
+
+const JE_byte CryptKey[10] = /* [1..10] */
+{
+    15,50,89,240,147,34,86, 9,32,208
+};
+
+const JE_KeySettingType DefaultKeySettings =
+{
+    72,80,75,77,57,28,29,56
+};
+
+const char DefaultHighScoreNames[34][23] = /* [1..34] of string [22] */
+{/*1P*/
+/*TYR*/     "The Prime Chair",    /*13*/
+            "Transon Lohk",
+            "Javi Onukala",
+            "Mantori",
+            "Nortaneous",
+            "Dougan",
+            "Reid",
+            "General Zinglon",
+            "Late Gyges Phildren",
+            "Vykromod",
+            "Beppo",
+            "Borogar",
+            "ShipMaster Carlos",
+
+/*OTHER*/   "Jill",               /*5*/
+            "Darcy",
+            "Jake Stone",
+            "Malvineous Havershim",
+            "Marta Louise Velasquez",
+
+/*JAZZ*/    "Jazz Jackrabbit",    /*3*/
+            "Eva Earlong",
+            "Devan Shell",
+
+/*OMF*/     "Crystal Devroe",     /*11*/
+            "Steffan Tommas",
+            "Milano Angston",
+            "Christian",
+            "Shirro",
+            "Jean-Paul",
+            "Ibrahim Hothe",
+            "Angel",
+            "Cossette Akira",
+            "Raven",
+            "Hans Kreissack",
+
+/*DARE*/    "Tyler",              /*2*/
+            "Rennis the Rat Guard"
+};
+
+const char DefaultTeamNames[22][25] = /* [1..22] of string [24] */
+{
+    "Jackrabbits",
+    "Team Tyrian",
+    "The Elam Brothers",
+    "Dare to Dream Team",
+    "Pinball Freaks",
+    "Extreme Pinball Freaks",
+    "Team Vykromod",
+    "Epic All-Stars",
+    "Hans Keissack's WARriors",
+    "Team Overkill",
+    "Pied Pipers",
+    "Gencore Growlers",
+    "Microsol Masters",
+    "Beta Warriors",
+    "Team Loco",
+    "The Shellians",
+    "Jungle Jills",
+    "Murderous Malvineous",
+    "The Traffic Department",
+    "Clan Mikal",
+    "Clan Patrok",
+    "Carlos' Crawlers"
+};
+
+
+const JE_EditorItemAvailType InitialItemAvail =
+{
+    1,1,1,0,0,1,1,0,1,1,1,1,1,0,1,0,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0, /*Front/Rear Weapons  1-38*/
+    0,0,0,0,0,0,0,0,0,0,1,                                                           /*Fill*/
+    1,0,0,0,0,1,0,0,0,1,1,0,1,0,0,0,0,0,                                             /*Sidekicks          51-68*/
+    0,0,0,0,0,0,0,0,0,0,0,                                                           /*Fill*/
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,                                                   /*Special Weapons    81-93*/
+    0,0,0,0,0                                                                        /*Fill*/
+};
+
+/*  Last 2 bytes = Word
+
+    Max Value = 1680
+    X div  60 = Armor  (1-28)
+    X div 168 = Shield (1-12)
+    X div 280 = Engine (1-06)
+*/
+
+
+JE_boolean smoothies[9]; /* [1..9] */
+JE_byte StarShowVGASpecialCode;
+
+/* Stars */
+struct
+{
+    JE_byte SC;
+    JE_word SLoc;
+    JE_word SMov;
+} StarDat[MaxStars]; /* [1..Maxstars] */
+JE_word StarY;
+
+
+/* CubeData */
+JE_word lastcubemax, cubemax;
+JE_word cubelist[4]; /* [1..4] */
+
+/* High-Score Stuff */
+JE_boolean gamehasrepeated;
+
+/* Difficulty */
+JE_shortint DifficultyLevel, OldDifficultyLevel, InitialDifficulty;
+
+/* Player Stuff */
+JE_longint score, score2;
+
+JE_integer    power, LastPower, PowerAdd;
+JE_PItemsType PItems, PItemsPlayer2, PItemsBack, PItemsBack2;
+JE_shortint   shield, ShieldMax, ShieldSet;
+JE_shortint   shield2, ShieldMax2;
+JE_integer    ArmorLevel, ArmorLevel2;
+JE_byte       ShieldWait, ShieldT;
+
+JE_byte          ShotRepeat[11], ShotMultiPos[11]; /* [1..11] */  /* 7,8 = Superbomb */
+JE_byte          PortConfig[10]; /* [1..10] */
+JE_boolean       PortConfigDone;
+JE_PortPowerType PortPower, LastPortPower;
+
+JE_boolean ResetVersion;
+
+/* Level Data */
+char    LastLevelName[11], LevelName[11]; /* string [10] */
+JE_byte Mainlevel, NextLevel, SaveLevel;   /*Current Level #*/
+
+/* Keyboard Junk */
+JE_KeySettingType KeySettings;
+
+/* Configuration */
+JE_shortint LevelFilter, LevelFilterNew, LevelBrightness, LevelBrightnessChg;
+JE_boolean  FiltrationAvail, FilterActive, FilterFade, FilterFadeStart;
+
+JE_boolean GameJustLoaded;
+
+JE_boolean GalagaMode;
+
+JE_boolean ExtraGame;
+
+JE_boolean TwoPlayerMode, TwoPlayerLinked, OnePlayerAction, SuperTyrian, TrentWin;
+JE_byte    SuperArcadeMode;
+
+JE_byte    SuperArcadePowerup;
+
+JE_real LinkGunDirec;
+JE_byte PlayerDevice1, PlayerDevice2;
+JE_byte InputDevice1, InputDevice2;
+
+JE_byte SecretHint;
+JE_byte Background3Over;
+JE_byte Background2Over;
+JE_byte GammaCorrection;
+JE_boolean SuperPause,
+           ExplosionTransparent,
+           YouAreCheating,
+           DisplayScore,
+           SoundHasChanged,
+           Background2, SmoothScroll, Wild, SuperWild, StarActive,
+           TopEnemyOver,
+           SkyEnemyOverALL,
+           Background2NotTransparent,
+           tempb;
+JE_byte temp;
+JE_word tempw;
+
+JE_byte VersionNum;   /*SW 1.0 and SW/Reg 1.1 = 0 or 1
+                        EA 1.2 = 2*/
+
+JE_byte    FastPlay;
+JE_boolean PentiumMode;
+
+/* Savegame files */
+JE_boolean PlayerPasswordInput;
+JE_byte    InputDevice;  /* 0=Mouse   1=Joystick   2=Gravis GamePad */
+JE_byte    GameSpeed;
+JE_byte    ProcessorType;  /* 1=386  2=486  3=Pentium Hyper */
+
+JE_SaveFilesType SaveFiles; /*array[1..savefilesnum] of savefiletype;*/
+JE_word savefilesseg, savefilesofs;
+JE_SaveGameTemp SaveTemp;
+JE_word savetempofs, savetempseg;
+
+JE_word EditorLevel;   /*Initial value 800*/
+
+JE_word x;
+
 
 const JE_byte StringCryptKey[10] = {99, 204, 129, 63, 255, 71, 19, 25, 62, 1};
 
--- src/config.h
+++ src/config.h
@@ -23,12 +23,132 @@
 
 #include <stdio.h>
 
-void JE_decryptstring( JE_string s, JE_byte len );
+
+#define SaveFilesNum (11 * 2)
+
+#define MaxStars 100
+
+#define SaveFileSize  (sizeof(SaveGameTemp) - 4)
+#define SaveFilesSize (sizeof(JE_SaveFilesType))
+
+typedef JE_byte JE_KeySettingType[8]; /* [1..8] */
+typedef JE_byte JE_PItemsType[12]; /* [1..12] */
+
+typedef JE_byte JE_EditorItemAvailType[100]; /* [1..100] */
+
+typedef struct
+{
+    JE_word       encode;
+    JE_word       level;
+    JE_PItemsType items;
+    JE_longint    score;
+    JE_longint    score2;
+    char          levelname[10]; /* string [9]; */
+    JE_char       name[14]; /* [1..14] */
+    JE_byte       cubes;
+    JE_byte       power[2]; /* [1..2] */
+    JE_byte       episode;
+    JE_PItemsType lastitems;
+    JE_byte       difficulty;
+    JE_byte       secrethint;
+    JE_byte       input1;
+    JE_byte       input2;
+    JE_boolean    gamehasrepeated;   /*See if you went from one episode to another*/
+    JE_byte       initialdifficulty;
+
+    /* High Scores - Each episode has both sets of 1&2 player selections - with 3 in each */
+    JE_longint    highscore1,
+                  highscore2;
+    char          highscorename[30]; /* string [29] */
+    JE_byte       highscorediff;
+} JE_SaveFileType;
+
+typedef JE_SaveFileType JE_SaveFilesType[SaveFilesNum]; /* [1..savefilesnum] */
+typedef JE_byte JE_SaveGameTemp[sizeof(JE_SaveFilesType) + 4 + 100]; /* [1..sizeof(savefilestype) + 4 + 100] */
+
+typedef JE_byte JE_PortPowerType[7]; /* [1..7] */
+
+#ifndef NO_EXTERNS
+extern const JE_byte CryptKey[10];
+extern const JE_KeySettingType DefaultKeySettings;
+extern const char DefaultHighScoreNames[34][23];
+extern const char DefaultTeamNames[22][25];
+extern const JE_EditorItemAvailType InitialItemAvail;
+extern JE_boolean smoothies[9];
+extern JE_byte StarShowVGASpecialCode;
+extern struct
+{
+    JE_byte SC;
+    JE_word SLoc;
+    JE_word SMov;
+} StarDat[MaxStars];
+extern JE_word StarY;
+extern JE_word lastcubemax, cubemax;
+extern JE_word cubelist[4];
+extern JE_boolean gamehasrepeated;
+extern JE_shortint DifficultyLevel, OldDifficultyLevel, InitialDifficulty;
+extern JE_longint score, score2;
+extern JE_integer power, LastPower, PowerAdd;
+extern JE_PItemsType PItems, PItemsPlayer2, PItemsBack, PItemsBack2;
+extern JE_shortint shield, ShieldMax, ShieldSet;
+extern JE_shortint shield2, ShieldMax2;
+extern JE_integer ArmorLevel, ArmorLevel2;
+extern JE_byte ShieldWait, ShieldT;
+extern JE_byte ShotRepeat[11], ShotMultiPos[11];
+extern JE_byte PortConfig[10];
+extern JE_boolean PortConfigDone;
+extern JE_PortPowerType PortPower, LastPortPower;
+extern JE_boolean ResetVersion;
+extern char LastLevelName[11], LevelName[11];
+extern JE_byte Mainlevel, NextLevel, SaveLevel;
+extern JE_KeySettingType KeySettings;
+extern JE_shortint LevelFilter, LevelFilterNew, LevelBrightness, LevelBrightnessChg;
+extern JE_boolean FiltrationAvail, FilterActive, FilterFade, FilterFadeStart;
+extern JE_boolean GameJustLoaded;
+extern JE_boolean GalagaMode;
+extern JE_boolean ExtraGame;
+extern JE_boolean TwoPlayerMode, TwoPlayerLinked, OnePlayerAction, SuperTyrian, TrentWin;
+extern JE_byte SuperArcadeMode;
+extern JE_byte SuperArcadePowerup;
+extern JE_real LinkGunDirec;
+extern JE_byte PlayerDevice1, PlayerDevice2;
+extern JE_byte InputDevice1, InputDevice2;
+extern JE_byte SecretHint;
+extern JE_byte Background3Over;
+extern JE_byte Background2Over;
+extern JE_byte GammaCorrection;
+extern JE_boolean SuperPause, ExplosionTransparent, YouAreCheating, DisplayScore, SoundHasChanged, Background2, SmoothScroll, Wild, SuperWild, StarActive, TopEnemyOver,SkyEnemyOverALL, Background2NotTransparent, tempb;
+extern JE_byte temp;
+extern JE_word tempw;
+extern JE_byte VersionNum;
+extern JE_byte FastPlay;
+extern JE_boolean PentiumMode;
+extern JE_boolean PlayerPasswordInput;
+extern JE_byte InputDevice;
+extern JE_byte GameSpeed;
+extern JE_byte ProcessorType;
+extern JE_SaveFilesType SaveFiles;
+extern JE_word savefilesseg, savefilesofs;
+extern JE_SaveGameTemp SaveTemp;
+extern JE_word savetempofs, savetempseg;
+extern JE_word EditorLevel;
+extern JE_word x;
+
+extern const JE_byte StringCryptKey[10];
+#endif
+
+void JE_InitProcessorType( void );
+void JE_setnewgamespeed( void );
+void JE_loadconfiguration( void );
+void JE_saveconfiguration( void );
 void JE_readcryptln( FILE* f, JE_string s );
 void JE_skipcryptln( FILE* f );
 
-#ifndef NO_EXTERNS
-extern const JE_byte StringCryptKey[10];
-#endif
+void JE_SetupStars( void );
+
+void JE_savegame( JE_byte slot, JE_string name );
+void JE_loadgame( JE_byte slot );
+
+void JE_decryptstring( JE_string s, JE_byte len );
 
 #endif /* CONFIG_H */
