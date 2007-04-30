437af628247dc13465841ed1be811b47a3320b4a
Yuri Kunde Schlesner <yuriks@yuriks.net>
2007-04-30 17:01:26 +0000

    Removed (now) uneeded workaround on the main menu

--- src/tyrian2.c
+++ src/tyrian2.c
@@ -1986,10 +1986,6 @@ void JE_titleScreen( JE_boolean animate )
 							quit = TRUE;
 							break;
 					}
-					if (menu != 4) /* Tweak added to prevent fadeIn when selecting Ordering Info. */
-					{
-						redraw = TRUE;
-					}
 					break;
 				case SDLK_ESCAPE:
 					quit = TRUE;
