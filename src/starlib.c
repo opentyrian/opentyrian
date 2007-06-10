a918da02a1565728a89dc0a3eb6d46389f0ff4c7
Yuri Kunde Schlesner <yuriks@yuriks.net>
2007-06-10 05:55:44 +0000

    Ported starlib except the main loop

--- src/starlib.c
+++ src/starlib.c
@@ -27,6 +27,9 @@
 JE_boolean run;
 struct JE_StarType star[starlib_MAX_STARS];
 
+JE_integer tempW;
+JE_integer tempX, tempY;
+
 JE_byte setup;
 JE_word stepCounter;
 
@@ -53,12 +56,13 @@ JE_byte pColor;
 
 void JE_starlib_main( void )
 {
-	STUB(JE_starlib_main);	
+	STUB(JE_starlib_main);
 }
 
 void JE_makeGray( void )
 {
-	STUB(JE_makeGray);	
+	/* YKS: Not used anywhere. */
+	STUB(JE_makeGray);
 }
 
 void JE_wackyCol( void )
@@ -69,20 +73,152 @@ void JE_wackyCol( void )
 
 void JE_starlib_init( void )
 {
-	STUB(JE_starlib_init);
+	/* RANDOMIZE; */
+	for (x = 0; x < starlib_MAX_STARS; x++)
+	{
+		star[x].spX = (rand() % 64000) - 32000;
+		star[x].spY = (rand() % 40000) - 20000;
+		star[x].spZ = x+1;
+	}
 }
 
 void JE_resetValues( void )
 {
-	STUB(JE_resetValues);	
+	nsp2 = 1;
+	nspVar2Inc = 1;
+	nspVarInc = 0.1;
+	nspVarVarInc = 0.0001;
+	nsp = 0;
+	pColor = 32;
+	speed = 2;
+	speedChange = 0;
 }
 
 void JE_changeSetup( JE_byte setupType )
 {
-	STUB(JE_changeSetup);	
+	stepCounter = 0;
+	changeTime = (rand() % 1000);
+
+	if (setupType > 0)
+	{
+		setup = setupType;
+	} else {
+		setup = rand() % (MAX_TYPES + 1);
+	}
+
+	if (setup == 1)
+	{
+		nspVarInc = 0.1;
+	}
+	if (nspVarInc > 2.2)
+	{
+		nspVarInc = 0.1;
+	}
 }
 
 void JE_newStar( void )
 {
-	STUB(JE_newStar);	
+	if (setup == 0)
+	{
+		tempX = (rand() % 64000) - 32000;
+		tempY = (rand() % 40000) - 20000;
+	} else {
+		nsp = nsp + nspVarInc; /* YKS: < lol */
+		switch (setup)
+		{
+			case 1:
+				tempX = (int)(sin(nsp / 30) * 20000);
+				tempY = (rand() % 40000) - 20000;
+				break;
+			case 2:
+				tempX = (int)(cos(nsp) * 20000);
+				tempY = (int)(sin(nsp) * 20000);
+				break;
+			case 3:
+				tempX = (int)(cos(nsp * 15) * 100) * ((int)(nsp / 6) % 200);
+				tempY = (int)(sin(nsp * 15) * 100) * ((int)(nsp / 6) % 200);
+				break;
+			case 4:
+				tempX = (int)(sin(nsp / 60) * 20000);
+				tempY = (int)(cos(nsp) * (int)(sin(nsp / 200) * 300) * 100);
+				break;
+			case 5:
+				tempX = (int)(sin(nsp / 2) * 20000);
+				tempY = (int)(cos(nsp) * (int)(sin(nsp / 200) * 300) * 100);
+				break;
+			case 6:
+				tempX = (int)(sin(nsp) * 40000);
+				tempY = (int)(cos(nsp) * 20000);
+				break;
+			case 8:
+				tempX = (int)(sin(nsp / 2) * 40000);
+				tempY = (int)(cos(nsp) * 20000);
+				break;
+			case 7:
+				tempX = rand() % 65535;
+				if ((rand() % 2) == 0)
+				{
+					tempY = (int)(cos(nsp / 80) * 10000) + 15000;
+				} else {
+					tempY = 50000 - (int)(cos(nsp / 80) * 13000);
+				}
+				break;
+			case 9:
+				nsp2 += nspVar2Inc;
+				if ((nsp2 == 65535) || (nsp2 == 0))
+				{
+					nspVar2Inc = -nspVar2Inc;
+				}
+				tempX = (int)(cos(sin(nsp2 / 10.) + (nsp / 500)) * 32000);
+				tempY = (int)(sin(cos(nsp2 / 10.) + (nsp / 500)) * 30000);
+				break;
+			case 10:
+				nsp2 += nspVar2Inc;
+				if ((nsp2 == 65535) || (nsp2 == 0))
+				{
+					nspVar2Inc = -nspVar2Inc;
+				}
+				tempX = (int)(cos(sin(nsp2 / 5.) + (nsp / 100)) * 32000);
+				tempY = (int)(sin(cos(nsp2 / 5.) + (nsp / 100)) * 30000);
+				break;;
+			case 11:
+				nsp2 += nspVar2Inc;
+				if ((nsp2 == 65535) || (nsp2 == 0))
+				{
+					nspVar2Inc = -nspVar2Inc;
+				}
+				tempX = (int)(cos(sin(nsp2 / 1000.) + (nsp / 2)) * 32000);
+				tempY = (int)(sin(cos(nsp2 / 1000.) + (nsp / 2)) * 30000);
+				break;
+			case 12:
+				if (nsp != 0)
+				{
+					nsp2 += nspVar2Inc;
+					if ((nsp2 == 65535) || (nsp2 == 0))
+					{
+						nspVar2Inc = -nspVar2Inc;
+					}
+					tempX = (int)(cos(sin(nsp2 / 2.) / (sqrt(abs(nsp)) / 10. + 1) + (nsp2 / 100.)) * 32000);
+					tempY = (int)(sin(cos(nsp2 / 2.) / (sqrt(abs(nsp)) / 10. + 1) + (nsp2 / 100.)) * 30000);
+				}
+				break;
+			case 13:
+				if (nsp != 0)
+				{
+					nsp2 += nspVar2Inc;
+					if ((nsp2 == 65535) || (nsp2 == 0))
+					{
+						nspVar2Inc = -nspVar2Inc;
+					}
+					tempX = (int)(cos(sin(nsp2 / 10.) / 2 + (nsp / 20)) * 32000);
+					tempY = (int)(sin(sin(nsp2 / 11.) / 2 + (nsp / 20)) * 30000);
+				}
+				break;
+			case 14:
+				nsp2 += nspVar2Inc;
+				tempX = (int)((sin(nsp) + cos(nsp2 / 1000.) * 3) * 12000);
+				tempY = (int)(cos(nsp) * 10000) + nsp2;
+				break;
+		}
+	}
 }
