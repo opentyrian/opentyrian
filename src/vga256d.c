fcb72524f0547513ef06647cf8d78d773bc9cfcd
Yuri Kunde Schlesner <yuriks@yuriks.net>
2007-09-29 02:16:27 +0000

    Fixed fullscreen

--- src/vga256d.c
+++ src/vga256d.c
@@ -97,11 +97,13 @@ void JE_initVGA256( void )
 
 void set_fullscreen( bool full )
 {
-	if ((VGAScreen = VGAScreenSeg = SDL_SetVideoMode(320, 200, 8, SDL_SWSURFACE | SDL_HWPALETTE | (full ? SDL_FULLSCREEN : 0))) == 0);
+	if ((VGAScreen = VGAScreenSeg = SDL_SetVideoMode(320, 200, 8, SDL_SWSURFACE | SDL_HWPALETTE | (full ? SDL_FULLSCREEN : 0))) == 0)
 	{
 		printf("Display initialization failed: %s\n", SDL_GetError());
 		exit(1);
 	}
+
+	SDL_ShowCursor(!full);
 }
 
 void JE_initVGA256X( void )
