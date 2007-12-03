1ac43abc04856261900bf184ae16cdaef4cf477d
Yuri Kunde Schlesner <yuriks@yuriks.net>
2007-12-03 19:07:33 +0000

    Less beer and more coins and bling bling on the scroller

--- src/scroller.c
+++ src/scroller.c
@@ -20,25 +20,16 @@
 #include "opentyr.h"
 #include "scroller.h"
 
-#include "backgrnd.h"
 #include "fonthand.h"
 #include "joystick.h"
 #include "jukebox.h"
 #include "keyboard.h"
 #include "newshape.h"
 #include "nortsong.h"
-#include "nortvars.h"
-#include "picload.h"
 #include "starfade.h"
 #include "varz.h"
 #include "vga256d.h"
 
-#include "SDL.h"
-
-
-#ifndef min
-#define min(a,b) ((a) < (b) ? (a) : (b))
-#endif
 
 const struct about_text_type about_text[] =
 {
@@ -57,10 +48,15 @@ const struct about_text_type about_text[] =
 	{0x10, "----- ~Developers~ -----"}, /* in alphabetical order */
 	{0x00, ""},
 	{0x03, "MindlessXD"},
-	{0x00, ""},
 	{0x04, "syntaxglitch"},
-	{0x00, ""},
 	{0x07, "yuriks"},
+	{0x00, ""},
+	{0x05, "Thanks to the ~AdPlug~ team for donating"},
+	{0x05, "the FM synthesis and LDS loading code."},
+	{0x00, ""},
+	{0x00, ""},
+	{0x12, "And a big thanks to ~Jason Emery~"},
+	{0x12, "for making all this possible."},
 	{0x00, ""},
 	{0x00, ""},
 	{0x00, "This is line color test ~0~."},
@@ -81,17 +77,25 @@ const struct about_text_type about_text[] =
 	{0x0f, "This is line color test ~F~."},
 	{0x00, ""},
 	{0x00, ""},
-	{0x12, "And a big thanks to ~Jason~ ~Emery~"},
-	{0x12, "for making this possible."},
-	{0x00, ""},
-	{0x00, ""},
-	{0x00, ""},
 	{0x00, ""},
 	{0x00, "Press a key to leave."},
 	{0x00, NULL}
 };
 
 #define BEER_SHAPE 241
+
+struct coin_def_type {
+	int shape_num;
+	int frame_count;
+	bool reverse_anim;
+};
+
+#define MAX_COINS 20
+struct coin_def_type coin_defs[] =
+{
+	{1, 6}, {7, 6}, {20, 6}, {26, 6}, // Coins
+	{14, 5, true}, {32, 5, true}, {51, 5, true} // Gems
+};
 
 /* Text is an array of strings terminated by a NULL */
 void scroller_sine( const struct about_text_type text[] )
@@ -101,9 +105,22 @@ void scroller_sine( const struct about_text_type text[] )
 	int y = 0;
 	bool fade_in = true;
 	
-	struct { int x, y, ay, vx, vy; } beer[5];
+	struct coin_type { int x, y, vel, type, cur_frame; bool backwards; } coins[MAX_COINS];
 	
-	memset(beer, 0, sizeof(beer));
+	// struct { int x, y, ay, vx, vy; } beer[5];
+	//memset(beer, 0, sizeof(beer));
+
+	for (int i = 0; i < MAX_COINS; i++)
+	{
+		coins[i].x = rand() % (surface_width - 12);
+		coins[i].y = rand() % (surface_height - 20 - 14);
+
+		coins[i].vel = (rand() % 4) + 1;
+		coins[i].type = rand() % COUNTOF(coin_defs);
+		coins[i].cur_frame = rand() % coin_defs[coins[i].type].frame_count;
+		coins[i].backwards = false;
+	}
+
 	
 	JE_fadeBlack(10);
 	
@@ -118,12 +135,20 @@ void scroller_sine( const struct about_text_type text[] )
 		
 		JE_clr256();
 		
+		for (int i = 0; i < MAX_COINS/2; i++)
+		{
+			struct coin_type *coin = &coins[i];
+			JE_drawShape2(coin->x, coin->y, coin_defs[coin->type].shape_num + coin->cur_frame, eShapes5);
+		}
+
 		for (int i = 0; i < visible_lines; i++)
 		{
 			if (current_line + i >= 0)
 			{
 				if (text[current_line + i].text == NULL)
-					goto no_more_text;
+				{
+					break;
+				}
 				
 				int line_x = JE_fontCenter(text[i + current_line].text, SMALL_FONT_SHAPES);
 				int line_y = i * 15 - y;
@@ -131,7 +156,9 @@ void scroller_sine( const struct about_text_type text[] )
 				JE_outTextAdjust(line_x, line_y, text[i + current_line].text, text[i + current_line].effect & 0x0f, -4, SMALL_FONT_SHAPES, false);
 				
 				if (text[i + current_line].effect & 0x10)
+				{
 					for (int j = 0; j < 15; j++)
+					{
 						if (line_y + j >= 10 && line_y + j <= surface_height - 10)
 						{
 							int foo = sin((((line_y + j) / 2) % 13) / 6.5f * M_PI) * 3;
@@ -139,23 +166,71 @@ void scroller_sine( const struct about_text_type text[] )
 									&((Uint8 *)VGAScreenSeg->pixels)[VGAScreenSeg->pitch * (line_y + j)],
 									VGAScreenSeg->pitch);
 						}
+					}
+				}
 			}
 		}
-no_more_text:
 		
 		y++;
 		y %= 15;
 		if (y == 0)
 		{
 			if (current_line < 0 || text[current_line].text != NULL)
+			{
 				current_line++;
-			else
+			} else {
 				current_line = -visible_lines;
+			}
 		}
+
+		for (int i = MAX_COINS/2; i < MAX_COINS; i++)
+		{
+			struct coin_type *coin = &coins[i];
+			JE_drawShape2(coin->x, coin->y, coin_defs[coin->type].shape_num + coin->cur_frame, eShapes5);
+		}
+
+		JE_bar(0, 0, surface_width - 1, 14, 0);
+		JE_bar(0, surface_height - 14, surface_width - 1, surface_height - 1, 0);
 		
-		JE_bar(0, 0, surface_width - 1, 9, 0);
-		JE_bar(0, surface_height - 10, surface_width - 1, surface_height - 1, 0);
-		
+		for (int i = 0; i < MAX_COINS; i++)
+		{
+			struct coin_type *coin = &coins[i];
+
+			if (coin->backwards)
+			{
+				coin->cur_frame--;
+			} else {
+				coin->cur_frame++;
+			}
+			if (coin->cur_frame == coin_defs[coin->type].frame_count)
+			{
+				if (coin_defs[coin->type].reverse_anim)
+				{
+					coin->backwards = true;
+					coin->cur_frame -= 2;
+				} else {
+					coin->cur_frame = 0;
+				}
+			}
+			if (coin->cur_frame == -1)
+			{
+				coin->cur_frame = 1;
+				coin->backwards = false;
+			}
+
+			coin->y += coin->vel;
+			if (coin->y > surface_height - 14)
+			{
+				coin->x = rand() % (surface_width - 12);
+				coin->y = 0;
+
+				coin->vel = (rand() % 4) + 1;
+				coin->type = rand() % COUNTOF(coin_defs);
+				coin->cur_frame = rand() % coin_defs[coin->type].frame_count;
+			}
+		}
+
+		/*
 		for (int i = 0; i < COUNTOF(beer); i++)
 		{
 			while (beer[i].vx == 0)
@@ -169,20 +244,25 @@ no_more_text:
 			beer[i].vy++;
 			
 			if (beer[i].x + beer[i].vx > surface_width - 24 || beer[i].x + beer[i].vx < 0) // check if the beer hit the sides
+			{
 				beer[i].vx = -beer[i].vx;
+			}
 			beer[i].x += beer[i].vx;
 			
 			if (beer[i].y + beer[i].vy > surface_height - 28) // check if the beer hit the bottom
 			{
 				beer[i].vy = -(beer[i].vy * 15 / 16) + (rand() % 3 - 1); // make sure the beer doesn't bounce too high
 				if ((beer[i].vy) > -10) // make sure the beer bounces!
+				{
 					beer[i].vy--;
+				}
 			}
 			beer[i].y += beer[i].vy;
 			
 			JE_drawShape2x2(beer[i].x, beer[i].y, BEER_SHAPE, eShapes5);
 		}
-		
+		*/
+
 		JE_showVGA();
 		
 		if (fade_in)
@@ -192,9 +272,11 @@ no_more_text:
 			JE_setPalette(254, 63, 63, 63);
 		}
 		
-		int delaycount_temp;
-		if ((delaycount_temp = target - SDL_GetTicks()) > 0)
+		int delaycount_temp = target - SDL_GetTicks();
+		if (delaycount_temp > 0)
+		{
 			SDL_Delay(delaycount_temp);
+		}
 	}
 	
 	JE_fadeBlack(10);
